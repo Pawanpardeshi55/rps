560051dae322faa53d45689b8f96e9cdb868dbe1
Pawansing Pardeshi <51014216+Pawanpardeshi55@users.noreply.github.com>
2020-07-27 18:08:49 +0530

    Update wsgi.py

--- c/wsgi.py
+++ c/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channels_obstruction.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "c.settings")
 
 application = get_wsgi_application()
